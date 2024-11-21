47765b3b08725600fdf881f20183c4e07a636c56
jeswanth <jachu.k1212@gmail.com>
2024-11-21 09:51:14 +0530

    Full_Version-with corrections

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -8,56 +8,22 @@ import HeaderFilter from "./Components/HeaderFilter";
 import RecommendationChart from "./Components/RecommendationChart";
 import TrackedFrequency from "./Components/TrackedFrequency";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <SideBar />
-//       {/* <Dashboard /> */}
-//       <HeaderFilter />
-//       <HeaderBar />
-//       <RecommendationChart />
-//       <FrequencyTable />
-//       <TrackedFrequency />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// import React from "react";
-// import SideBar from "./SideBar";
-// import HeaderBar from "./HeaderBar";
-// import HeaderFilter from "./HeaderFilter";
-// import RecommendationChart from "./RecommendationChart";
-// import FrequencyTable from "./FrequencyTable";
-// import TrackedFrequency from "./TrackedFrequency";
-
-const App = () => {
+function App() {
   return (
     <div className="flex h-screen bg-gray-900">
-      {/* Sidebar */}
       <SideBar />
 
       {/* Main Content Section */}
       <div className="w-[95%] h-screen overflow-y-auto">
-        {/* Header */}
         <HeaderBar />
-
-        {/* Filters Section */}
         <HeaderFilter />
-
-        {/* Recommendation Chart */}
         <RecommendationChart />
-
-        {/* Frequency Table */}
         <FrequencyTable />
-
-        {/* Tracked Frequency */}
         <TrackedFrequency />
       </div>
     </div>
   );
-};
+}
 
 export default App;
+
