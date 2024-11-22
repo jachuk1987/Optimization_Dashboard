c98395d42a7de18a3ad64b5350b8ad486c9ce752
jeswanth <jachu.k1212@gmail.com>
2024-11-22 21:58:01 +0530

    Complete_Corrections

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 
 import SideBar from "./Components/SideBar";
-import Dashboard from "./Components/Dashboard";
 import FrequencyTable from "./Components/FrequencyTable";
 import HeaderBar from "./Components/HeaderBar";
 import HeaderFilter from "./Components/HeaderFilter";
 import RecommendationChart from "./Components/RecommendationChart";
 import TrackedFrequency from "./Components/TrackedFrequency";
+import Dashboard from "./Components/Controlroom";
 
 function App() {
   return (
@@ -14,15 +14,18 @@ function App() {
       <SideBar />
 
       {/* Main Content Section */}
-      <div className="w-[95%] h-screen overflow-y-auto">
+      <div className="w-[96%] h-screen overflow-y-auto">
         <HeaderBar />
         <HeaderFilter />
         <RecommendationChart />
         <FrequencyTable />
         <TrackedFrequency />
+        
+        {/* <Dashboard/> */}
       </div>
     </div>
   );
 }
 
 export default App;
+
